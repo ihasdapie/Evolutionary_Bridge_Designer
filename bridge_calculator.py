adab853c27718b761d13cb6b5d321d3c135b6209
Brian Chen <brianchen.chen@mail.utoronto.ca>
2020-11-26 23:16:50 -0500

    update w/ local bridgelib

--- bridge_calculator.py
+++ bridge_calculator.py
@@ -1,18 +1,22 @@
-from bridgelib2 import *
+from bridgelib import *
 import math as math
 import random as random
 from tqdm import tqdm
 import copy
 
-# paper_thickness, height, length, flange_width, num_flange_layers_top, num_flange_layers_bottom, num_web_layers, web_dist, dia_dist
-# b1 = Bridge(1.27, 158, 1280, 100, 2, 2, 1, 50, 107)
-b1 = Bridge(1.27, 102.54, 1280, 105, 2, 2, 1, 75, 91.43)
+b1 = Bridge(1.27, 200, 980, 100, 2, 2, 75, 91.43)
+
+# def __init__(self, paper_thickness, height, length, flange_width, num_flange_layers, num_web_layers, web_dist, dia_dist):
 
 b1.report()
 
-b2 = copy.deepcopy(b1)
-# b2 = b1
+# run generative design on bridge
 
-b1.height = 3000
 
-b2.report()
+
+
+
+
+
+
+
